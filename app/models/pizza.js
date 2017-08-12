f0bc02d0ec2080067edf08ef0b052490c5c2e5b5
Miodkowy <michal.stasinski@poczta.fm>
2017-08-12 12:41:58 +0200

    pizze

--- app/models/pizza.js
+++ app/models/pizza.js
@@ -2,7 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   rank: DS.attr('number'),
-  price: DS.attr('number'),
+  price0: DS.attr('number'),
+  price1: DS.attr('number'),
+  price2: DS.attr('number'),
+  price3: DS.attr('number'),
+  price4: DS.attr('number'),
+  price5: DS.attr('number'),
+  sold: DS.attr('boolean'),
   name: DS.attr('string'),
   desc: DS.attr('string')
 
